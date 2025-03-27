dfe068d0aadec98347124e38c6bd1850164b8515
t-fukumoto <t-fukumoto@wwwave.jp>
2025-03-27 15:40:52 +0900

    ルート定義追加

--- jammate/src/main.py
+++ jammate/src/main.py
@@ -1,8 +1,9 @@
 import flet as ft
 from router import route_change
+from paths import Paths
 
 def main(page: ft.Page):
-    page.on_route_change = lambda e: route_change(page, e) 
-    page.go("/")
+    page.on_route_change = lambda e: route_change(page, e)
+    page.go(Paths.HOME)
 
 ft.app(target=main)
